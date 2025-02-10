31815fd686868b9e786e1754194f46328bb8108e
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2025-02-10 23:18:24 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,15 @@
+from pyrogram import Client, filters
+from config import BOT_TOKEN
+
+app = Client("skull_game_bot", bot_token=BOT_TOKEN)
+
+@app.on_message(filters.command("start"))
+async def start(client, message):
+    await message.reply_text("Welcome to Skull Game! Type /play to start.")
+
+@app.on_message(filters.command("play"))
+async def play(client, message):
+    game_url = "https://your-heroku-app.herokuapp.com/"
+    await message.reply_text(f"Click below to play!\n[Play Now]({game_url})", disable_web_page_preview=True)
+
+app.run()
